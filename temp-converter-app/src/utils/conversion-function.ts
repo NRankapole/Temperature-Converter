2a8948ae4cc7db71e0328de7f3693ab9bc9673f7
Your Name <NRankapole@dvtsoftware.com>
2026-03-02 01:12:40 +0200

    feat: Add function to round of to two decimals

--- temp-converter-app/src/utils/conversion-function.ts
+++ temp-converter-app/src/utils/conversion-function.ts
@@ -1,7 +1,11 @@
 export function convertToCelsius(fahrenheitTemp: number) {
-    return ((fahrenheitTemp - 32) * 5) / 9;
+    return roundToTwoDecimals(((fahrenheitTemp - 32) * 5) / 9);
 }
 
 export function convertToFahrenheit(celsiusTemp: number) {
-    return (celsiusTemp * 9) / 5 + 32;
+    return roundToTwoDecimals((celsiusTemp * 9) / 5 + 32);
+}
+
+function roundToTwoDecimals(num:number) {
+  return Math.round(num * 100) / 100;
 }
